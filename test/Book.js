412b42f742e94e1c6633e4f2850288b4dec02a11
Vibhav Yadav <vibhavyadav101@gmail.com>
2020-06-27 14:05:12 +0530

    test cases updated

--- test/Book.js
+++ test/Book.js
@@ -10,28 +10,62 @@ let baseUrl = '/api/v1/books';
 chai.use(chaiHttp);
 describe("Books", function(){
 
-  it("Should add Books in DB", (done) => {
+  it("Should Throw Validation Error", (done) => {
     let payload = {
-      "title": "let me code you",
-      "author": "Vibhav Yadav",
-      "isbn": ISBN,
+      "title": "",
+      "author": "",
+      "isbn": "",
       "released_date": "20201002"
     };
     chai.request(server)
     .post(baseUrl)
     .send(payload)
-    .end((err, res) => {
-      res.should.have.status(200);
-      console.log("Response Body:", res.body);
+    .end((err, result) => {
+      result.body.code.should.eq(400);
+      console.log("Errors:", result.body.data.error);
       done();
     });
   });
 
-  it ("Should Fecth all the Books", (done)=>{
+  it("Should add Books in DB", (done) => {
+    let payload = {
+      "title": "let me code you",
+      "author": "Vibhav Yadav",
+      "isbn": ISBN,
+      "released_date": "2020-10-02"
+    };
+    chai.request(server)
+    .post(baseUrl)
+    .send(payload)
+    .end((err, result) => {
+      result.body.code.should.eq(201);
+      console.log("Response Body:", result.body.data);
+      done();
+    });
+  });
+
+  it("Should return ISBN Unique Validation", (done) => {
+    let payload = {
+      "title": "let me code you",
+      "author": "Vibhav Yadav",
+      "isbn": ISBN,
+      "released_date": "2020-10-02"
+    };
+    chai.request(server)
+    .post(baseUrl)
+    .send(payload)
+    .end((err, result) => {
+      result.body.code.should.eq(400);
+      console.log("Error:", result.body.data.error);
+      done();
+    });
+  });
+
+  it ("Should Fetch all the Books", (done)=>{
     chai.request(server)
     .get(baseUrl)
     .end((err, result)=>{
-        result.should.have.status(200);
+        result.body.code.should.eq(200);
         console.log ("Got",result.body.data.length, " docs");
         done();
     });
@@ -40,9 +74,9 @@ describe("Books", function(){
   it ("Should Fetch Particular Book only", (done)=>{
     chai.request(server)
     .get(`${baseUrl}/${ISBN}`)
-    .end((err, result)=>{                    
-        result.should.have.status(200)
-        console.log(`Fetched Particlar Book with ISBN: ${ISBN}`, result.body);
+    .end((err, result)=>{                
+        result.body.code.should.eq(200);
+        console.log(`Fetched Particlar Book with ISBN: ${ISBN}`, result.body.data);
         done();
     });
   });
@@ -52,14 +86,14 @@ describe("Books", function(){
       "isbn": ISBN,
       "title": "vibhav sold the monk ferarri",
       "author": "monk",
-      "released_date": "20200310"
+      "released_date": "2020-03-10"
     }
     chai.request(server)
     .put(`${baseUrl}/${ISBN}`)
     .send(updatedBook)
-    .end((err, result)=>{                    
-      result.should.have.status(200);
-      console.log(`Updated Particlar Book with ISBN: ${ISBN}`, result.body);
+    .end((err, result)=>{               
+      result.body.code.should.eq(201);
+      console.log(`Updated Particlar Book with ISBN: ${ISBN}`, result.body.data);
       done();
     });
   });
@@ -67,11 +101,11 @@ describe("Books", function(){
   it ("should check data updated in DB", (done)=>{
     chai.request(server)
     .get(`${baseUrl}/${ISBN}`)
-    .end((err, result)=>{                    
-      result.should.have.status(200);  
+    .end((err, result)=>{                 
+      result.body.code.should.eq(200);
       result.body.data.isbn.should.eq(ISBN);
       result.body.data.author.should.eq("monk");
-      console.log(`Fetched Particlar Book with ISBN: ${ISBN}`, result.body)  ;  
+      console.log(`Fetched Particlar Book with ISBN: ${ISBN}`, result.body.data)  ;  
       done();;
     });
   });
@@ -79,9 +113,9 @@ describe("Books", function(){
   it("Should Delete Particular Book", (done)=>{
     chai.request(server)
     .delete(`${baseUrl}/${ISBN}`)
-    .end((err, result)=>{                    
-      result.should.have.status(200);            
-      console.log(`Deleted Particlar Book with ISBN: ${ISBN}`, result.body);    
+    .end((err, result)=>{               
+      result.body.code.should.eq(200);           
+      console.log(`Deleted Particlar Book with ISBN: ${ISBN}`, result.body.data);    
       done();
     });
   });
